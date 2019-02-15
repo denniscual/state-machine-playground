82e186f7a69081789138c094aac6b29a2d3b7e74
Dennis Cual <dennis.cual@ombori.com>
2019-02-15 21:55:10 +0800

    Create theme utility

---
+++ src/theme.js
@@ -0,0 +1,57 @@
+// @flow
+import React, { createContext, useReducer } from 'react'
+import { ThemeProvider as RootThemeProvider } from 'styled-components'
+import debug from 'debug'
+import type { Node, Element } from 'react'
+
+// Decorated debug tool
+const log = debug('themeReducer:dispatch')
+
+type ThemeAction = {
+  type: string,
+}
+
+// TODO: Add generic type instead of using Object for better flow check.
+function createThemesReducer(themes: Object) {
+  return function themesReducer(state: Object, action: ThemeAction) {
+    const { type } = action
+    // getting the theme based in the given action type (theme type).
+    const foundTheme = themes[type]
+    // If foundTheme is defined, then return it. Else, return state.
+    return foundTheme ? foundTheme : state
+  }
+}
+
+type ThemeDispatch = (action: ThemeAction) => void
+
+// Creating an abstraction layer for adding app theming. It is exposing an API Theme Provider and Context (Switch Dispatch Context).
+function initThemes(themes: Object) {
+  const themesReducer = createThemesReducer(themes)
+  const SwitchThemeContext = createContext<ThemeDispatch>(
+    (action: ThemeAction) => log(`Dispatching action type: ${action.type}`),
+  )
+
+  function ThemeProvider({
+    initTheme,
+    children,
+  }: {
+    initTheme: Object,
+    children: Node,
+  }) {
+    const [theme, dispatch] = useReducer(themesReducer, initTheme)
+    return (
+      <RootThemeProvider theme={theme}>
+        <SwitchThemeContext.Provider value={dispatch}>
+          {children}
+        </SwitchThemeContext.Provider>
+      </RootThemeProvider>
+    )
+  }
+
+  return {
+    ThemeProvider,
+    SwitchThemeContext,
+  }
+}
+
+export default initThemes
